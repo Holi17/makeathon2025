fb6433432ee12eb16c2cda159c03d535f52e302b
EXBLIUS <156904593+EXBLIUS@users.noreply.github.com>
2025-03-15 10:29:46 +0000

    Update moving.py

--- moving.py
+++ moving.py
@@ -1,5 +1,5 @@
 import state as st
-#import move as mv
+import move as mv
 import time
 import approaching as apState
 class Moving(st.State): # TODO: Insert moving logic
@@ -7,42 +7,42 @@ class Moving(st.State): # TODO: Insert moving logic
         print("Moving")
         time.sleep(2)
         self.robot.change_state(apState.Approaching(self.robot))
-        # try:
-        #     speed_set = 60
-        #     mv.setup()
-        #     running = True
-        #     while running:
-        #         mv.DriveStraight()
-        #         time.sleep(mv.FORWARD_LONG_TIME)
-        #         mv.motorStop()
+         try:
+             speed_set = 60
+             mv.setup()
+             running = True
+             while running:
+                 mv.DriveStraight()
+                 time.sleep(mv.FORWARD_LONG_TIME)
+                 mv.motorStop()
 
-        #         mv.DriveRight()
-        #         time.sleep(mv.SPIN_TIME)
-        #         mv.motorStop()
+                 mv.DriveRight()
+                 time.sleep(mv.SPIN_TIME)
+                 mv.motorStop()
 
-        #         mv.DriveStraight()
-        #         time.sleep(mv.FORWARD_SHORT_TIME)
-        #         mv.motorStop()
+                 mv.DriveStraight()
+                 time.sleep(mv.FORWARD_SHORT_TIME)
+                 mv.motorStop()
 
-        #         mv.DriveRight()
-        #         time.sleep(mv.SPIN_TIME)
-        #         mv.motorStop()
+                 mv.DriveRight()
+                 time.sleep(mv.SPIN_TIME)
+                 mv.motorStop()
 
-        #         mv.DriveStraight()
-        #         time.sleep(mv.FORWARD_LONG_TIME)
-        #         mv.motorStop()
+                 mv.DriveStraight()
+                 time.sleep(mv.FORWARD_LONG_TIME)
+                 mv.motorStop()
 
-        #         mv.DriveLeft()
-        #         time.sleep(mv.SPIN_TIME)
-        #         mv.motorStop()
+                 mv.DriveLeft()
+                 time.sleep(mv.SPIN_TIME)
+                 mv.motorStop()
 
-        #         mv.DriveStraight()
-        #         time.sleep(mv.FORWARD_SHORT_TIME)
-        #         mv.motorStop()
+                 mv.DriveStraight()
+                 time.sleep(mv.FORWARD_SHORT_TIME)
+                 mv.motorStop()
 
-        #         mv.DriveLeft()
-        #         time.sleep(mv.SPIN_TIME)
-        #         mv.motorStop()
+                 mv.DriveLeft()
+                 time.sleep(mv.SPIN_TIME)
+                 mv.motorStop()
 
-        # except KeyboardInterrupt:
+         except KeyboardInterrupt:
 	    #     mv.destroy()
