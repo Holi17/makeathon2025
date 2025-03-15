b10d070f311a804eab26c831e1262744a23a4991
EXBLIUS <156904593+EXBLIUS@users.noreply.github.com>
2025-03-15 11:33:14 +0000

    Turn.py

--- turn.py
+++ turn.py
@@ -1,7 +1,32 @@
 import moving as mvState
 import state as st
+import time
+import move as mv
 
 class Turn(st.State): # State which turns the robot if a collision happened (Transit to this if ultrasonic detects collision)
     def execute(self):
         print("Turn")
         self.robot.change_state(mvState.Moving(self.robot))
+        try:
+
+            mv.setup()
+            runing = True
+            while runing:
+                mv.DriveBackward()
+                mv.motorStop()
+                time.sleep(2)
+
+                mv.DriveLeft()
+                time.sleep(mv.SPIN_TIME)
+                mv.motorStop()
+
+                mv.DriveRight()
+                time.sleep(mv.SPIN_TIME)
+                mv.motorStop()
+
+                mv.DriveStraight()
+                time.sleep(mv.FORWARD_SHORT_TIME)
+                mv.motorStop()
+
+        except KeyboardInterrupt:
+            mv.destroy()
